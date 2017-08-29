b263be126d42b5550ff17ca0464c44c27b4219ff
HaykBrsoyan <hayk.brsoyan@picsart.com>
2017-08-29 12:31:07 +0400

    Update MPNativeAdDelegate.h

--- MoPubSDK/Native Ads/MPNativeAdDelegate.h
+++ MoPubSDK/Native Ads/MPNativeAdDelegate.h
@@ -14,6 +14,9 @@
 @protocol MPNativeAdDelegate <NSObject>
 
 @optional
+  
+- (void)willTrackImpressionForNativeAd:(MPNativeAd *)nativeAd;
+- (void)willTrackClickForNativeAd:(MPNativeAd *)nativeAd;
 
 /**
  * Sent when the native ad will present its modal content.
